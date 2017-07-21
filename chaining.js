d0ff520f475c3f4a44480fe68d600223e866def7
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-21 13:06:47 -0500

    Finished exercise, ready for review

--- chaining.js
+++ chaining.js
@@ -2,8 +2,7 @@
  * Created by beelarr on 7/21/17.
  */
 let integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-let solution = integers.sort((first, second)=>{return first - second}).splice(0, 10).map((a)=>{return ((a * 1.5) - 1)});
-console.log(solution)
+console.log(integers = integers.sort((first, second)=>{return first - second}).splice(0, 10).map((a)=>{return ((a * 1.5) - 1)}).reduce((total, num)=>{return total + num}));
 
 
 
